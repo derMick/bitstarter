2a747ae07b0e94aef493240a11ab1487fa6a6a06
derMick <dev@bartsch-hh.de>
2013-07-07 22:54:29 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
+buf = new Buffer();
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var file = fs.readFileSync('index.html');
-  file = buf.toString('utf8');
-  response.send(file);
+  buf = fs.readFileSync('index.html');
+  buf.toString('utf8');
+  response.send(buf);
   //response.send('Hello World2!');
 
 });
