8cf757d7162ada94b426d883e747831643a82938
derMick <dev@bartsch-hh.de>
2013-07-07 23:06:51 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buffer = new Buffer(fs.readFileSync('index.html'));
+  buffer = new Buffer(fs.readFileSync('./index.html', 'utf8'));
   buffer.toString('utf8');
   response.send(buffer);
   //response.send('Hello World2!');
